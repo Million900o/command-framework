5ec04d519492cc5ff87774b9af90b7f6be240f60
Million900o <apaolini900o@gmail.com>
2021-01-09 15:33:17 -0500

    fixed

--- src/lib/Handler.ts
+++ src/lib/Handler.ts
@@ -30,25 +30,34 @@ class Handler {
   }
 
   async runMessage(msg: any): Promise<undefined> {
-    if (!msg || !msg.content) return;
-    const prefix = this.options.defaultPrefix;
-    if (msg.content.startsWith(prefix)) {
-      const contentArray = msg.content.slice(prefix.length).split(' ').filter((e: string) => e)
-      const command = this.getCommand(contentArray);
-      if (command) {
-        try {
-          if (command.botPermissions) {
-            if (this.botPermissions[command.botPermissions].run(msg, command)) return;
+    try {
+      if (!msg || !msg.content) return;
+      const prefix = this.options.defaultPrefix;
+      if (msg.content.startsWith(prefix)) {
+        const contentArray = msg.content.slice(prefix.length).split(' ').filter((e: string) => e)
+        const command = this.getCommand(contentArray);
+        if (command) {
+          try {
+            if (command.botPermissions) {
+              if (this.botPermissions[command.botPermissions].run(msg, command)) return;
+            }
+            await command.run(msg, contentArray.slice(1));
+            return;
+          } catch (err) {
+            console.warn(err);
+            msg.channel.send(err)
+            return;
           }
-          await command.run(msg, contentArray.slice(1));
-          return;
-        } catch (err) {
-          console.warn(err);
-          msg.channel.send(err)
-          return;
-        }
+        } else return;
       } else return;
-    } else return;
+    } catch (err) {
+      const response = {
+        embed: {
+          title: `Err: ${err}`
+        }
+      };
+      msg.channel.send(response).catch(console.log);
+    }
   }
 
   getCommand(content: string[]): Command | undefined {
